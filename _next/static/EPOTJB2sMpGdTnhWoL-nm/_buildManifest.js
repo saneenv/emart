2c7d0487f351d5d8b550095ac2f0330537798d52
Saneen <saneen.v@datascapetek.com>
2026-02-16 16:25:13 +0530

    Updates

--- _next/static/EPOTJB2sMpGdTnhWoL-nm/_buildManifest.js
+++ _next/static/EPOTJB2sMpGdTnhWoL-nm/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/.//_next/:path+",
-        "destination": "/_next/:path+"
+        "source": "/emart//_next/:path+",
+        "destination": "/emart/_next/:path+"
       }
     ],
     "fallback": []
