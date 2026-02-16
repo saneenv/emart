38d173eb8451246ca9050f85929f37ae1440cc02
Saneen <saneen.v@datascapetek.com>
2026-02-16 16:28:26 +0530

    Updates

--- _next/static/sDE_J5qKi3geA3wIZU8lH/_buildManifest.js
+++ _next/static/sDE_J5qKi3geA3wIZU8lH/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/emart//_next/:path+",
-        "destination": "/emart/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
